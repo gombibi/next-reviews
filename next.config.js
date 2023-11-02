830479d3eeb8d53ade56ee3fb155d8273a75f392
gombibi <bb.style.on@gmail.com>
2023-11-02 15:10:54 +0900

    next config module.exports 수정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   //Static Export & Static Hosting
   //output 옵션: npm run build 시, .next 폴더가 아닌 out 폴더에 정적 파일들(html)이 생성됨
   //설정하지 않을 시 노드 서버를 통해 SSR, CSR을 모두 지원하는 hybrid 앱이 빌드됨(next start) -> 매우 복잡한 빌드 산출물
@@ -8,6 +8,4 @@ const nextConfig = {
   //Netlify에 out 폴더를 업로드하여 배포 가능
   //import from Git like Vercel or option to deploy manually by uploading a folder using drag and drop
   // output: 'export',
-}
-
-export default nextConfig
+};
