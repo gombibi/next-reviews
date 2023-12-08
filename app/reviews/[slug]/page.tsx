b855ad768b92fe68f04569c1d546010905f152f2
gombibi <bb.style.on@gmail.com>
2023-12-08 18:32:59 +0900

    typescript 버전 이슈 해결

--- app/reviews/[slug]/page.tsx
+++ app/reviews/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { ChatBubbleBottomCenterTextIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Heading from '@/components/Heading';
 import ShareButtons from '@/components/ShareButtons';
@@ -24,15 +25,15 @@ export async function generateStaticParams(): Promise<ReviewPageParams[]> {
   //  이 페이지에 사용해도 client side rendering이 필요한 ShareLinkButton(하위 컴포넌트)에 적용할 수 있으나,
   //  서버에서 동작하는 Node.js의 fs 모듈을 사용하고 있어(readdir(), readFile()) 명시할 수 없음.
   //  또한, 'use client'는 필요할 때만, 기능적으로 클라이언트 사이드를 사용하고 있는 컴포넌트에만 명시하는 것이 좋음
-  
+
   const slugs = await getSlugs();
   return slugs.map(slug => ({ slug }));
 }
 
 export async function generateMetadata({ params: { slug } }: ReviewPageProps) {
   const review = await getReview(slug);
-  if(!review){
-    notFound()
+  if (!review) {
+    notFound();
   }
   return {
     title: review.title,
@@ -54,6 +55,12 @@ export default async function ReviewPage({ params: { slug } }: ReviewPageProps) 
       </div>
       <Image src={review.image} alt='' priority width='640' height='360' className='mb-2 rounded' />
       <article dangerouslySetInnerHTML={{ __html: review.body }} className='max-w-screen-sm prose prose-slate' />
+      <section className='border-dashed border-t max-w-screen-sm mt-3 py-3'>
+        <h2 className='font-bold flex gap-2 items-center text-xl'>
+          <ChatBubbleBottomCenterTextIcon className='h-6 w-6' />
+          Comments
+        </h2>
+      </section>
     </>
   );
 }
